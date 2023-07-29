e9082e560c8a323b969ab2a5fd5463958ec41143
Adrian Richard <adrichard14@hotmail.com>
2023-07-28 22:04:22 -0300

    mudanças na paágina de dados do paciente + fix no pre-commit

--- pages/dados-paciente.tsx
+++ pages/dados-paciente.tsx
@@ -1,53 +1,59 @@
-import dynamic from "next/dynamic";
-import VerDadosPaciente from "../components/VerDadosPaciente/index";
+import dynamic from 'next/dynamic';
+import DetalhesPaciente from '../components/DetalhesPaciente';
+import { useState } from 'react';
 
 const DynamicTabComponent = dynamic(
-  () => import("../components/TabDadosPaciente"),
+  () => import('../components/TabDadosPaciente'),
   {
-    ssr: false,
-  },
+    ssr: false
+  }
 );
 const mockedPatients = [
   {
-    name: "Paciente teste 1",
-    prontuario: "188182388",
+    name: 'Paciente teste 1',
+    prontuario: '188182388',
     neutropenia: true,
-    neutropeniaFebril: true,
+    neutropeniaFebril: true
   },
   {
-    name: "Paciente teste 2",
-    prontuario: "498789",
+    name: 'Paciente teste 2',
+    prontuario: '498789',
     neutropenia: true,
-    neutropeniaFebril: true,
+    neutropeniaFebril: true
   },
   {
-    name: "Paciente teste 3",
-    prontuario: "2222",
+    name: 'Paciente teste 3',
+    prontuario: '2222',
     neutropenia: true,
-    neutropeniaFebril: false,
+    neutropeniaFebril: false
   },
   {
-    name: "Paciente teste 4",
-    prontuario: "23424",
+    name: 'Paciente teste 4',
+    prontuario: '23424',
     neutropenia: false,
-    neutropeniaFebril: false,
+    neutropeniaFebril: false
   },
   {
-    name: "Paciente teste 5",
-    prontuario: "34444",
+    name: 'Paciente teste 5',
+    prontuario: '34444',
     neutropenia: true,
-    neutropeniaFebril: true,
-  },
+    neutropeniaFebril: true
+  }
 ];
 const DadosPacientePage = () => {
+  const [selectedPatient, setSelectedPatient] = useState<Paciente>({});
+
   return (
     <>
       <section className="flex min-h-full flex items-center">
-        <div className="flex-1">
-          <DynamicTabComponent pacientes={mockedPatients} />
+        <div className="flex-1 mr-2">
+          <DynamicTabComponent
+            pacientes={mockedPatients}
+            setSelectedPatient={setSelectedPatient}
+          />
         </div>
-        <div className="flex-1">
-          <VerDadosPaciente />
+        <div className="flex-1 ml-2">
+          <DetalhesPaciente paciente={selectedPatient} />
         </div>
       </section>
     </>
