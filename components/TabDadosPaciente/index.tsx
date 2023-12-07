ea4370f81d9354ff2789c26604a1b77d12d9d010
Dannilo <dannilo.souz@gmail.com>
2023-12-07 13:06:54 -0300

    add: botão de add paciente

--- components/TabDadosPaciente/index.tsx
+++ components/TabDadosPaciente/index.tsx
@@ -6,6 +6,7 @@ import TabContents from "../TabContents/index";
 import { Tab, initTE } from "tw-elements";
 import TabList from "../TabList";
 import ItemListaPaciente from "../ItemListaPaciente";
+import router from "next/router";
 
 initTE({ Tab });
 
@@ -96,27 +97,39 @@ export default function TabDadosPaciente({
         </TabList>
         <div className="bg-[#DADADA] lista-pacientes_tab-content overflow-auto px-6">
           <TabContents tabId="tab-todos" active={true}>
-            <div className="flex bg-white rounded-xl p-2 mt-5 mb-4">
-              <div className="pr-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="h-5 w-5"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+            <div className="flex items-center">
+              <div className="flex bg-white rounded-xl p-2 mt-5 mb-4 w-full mr-4">
+                <div className="pr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="h-5 w-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+                <input
+                  className="block w-full focus:border-none active:border-none focus:outline-none"
+                  type="text"
+                  placeholder="Busque por nome ou prontuário do paciente"
+                  onChange={(e) => handleFilterPacientes(e.target.value)}
+                />
               </div>
-              <input
-                className="block w-full focus:border-none active:border-none focus:outline-none"
-                type="text"
-                placeholder="Busque por nome ou prontuário do paciente"
-                onChange={(e) => handleFilterPacientes(e.target.value)}
-              />
+              <button
+                className="flex w-8 h-auto rounded-full bg-orange-500 hover:bg-orange-400 items-center justify-center"
+                type="button"
+                title="Adicionar paciente"
+                onClick={() => {
+                  router.push("/pacientes");
+                }}
+              >
+                <span className="text-white text-xl font-bold">+</span>
+              </button>
             </div>
             <ul role="list" className="divide-y divide-gray-100">
               {listaPacientes?.map((paciente: Paciente) => (
