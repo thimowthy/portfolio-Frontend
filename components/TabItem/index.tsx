d56ab1bfb231120919726f8ee3894d80f4b4674d
Guilherme Santos <gjsantos@dcomp.ufs.br>
2024-03-10 23:20:54 -0300

    correcao inicial

--- components/TabItem/index.tsx
+++ components/TabItem/index.tsx
@@ -9,27 +9,48 @@ const TabItem = ({
   active,
   children,
   liClassName,
-  disabled,
+  disabled = false,
+  onSelect,
 }: {
   href: string;
   className?: string;
   title?: string;
-  active?: boolean;
-  children?: any;
+  active: boolean;
+  children?: React.ReactNode;
   liClassName?: string;
   disabled?: boolean;
+  onSelect?: (selectedTitle: string | React.ReactNode) => void;
 }) => {
+  const handleClick = (e: React.MouseEvent) => {
+    if (!disabled) {
+      e.preventDefault(); // Evita o redirecionamento automático
+      if (onSelect && typeof onSelect === "function") {
+        console.log("Active tab: ", title);
+        onSelect(href || children);
+      }
+    }
+  };
+
   return (
     <li role="presentation" className={liClassName}>
       <a
         href={`#${href}`}
-        className={`${className} ${disabled ? "disabled" : ""}`}
+        className={`${className} ${
+          disabled && active
+            ? "disabled data-[te-nav-active]:bg-[#EAEAEA]"
+            : disabled
+            ? "disabled"
+            : active
+            ? "active data-[te-nav-active]:bg-[#EAEAEA]"
+            : ""
+        } `}
         data-te-toggle="pill"
         data-te-target={`#${href}`}
         data-te-nav-active={active}
         role="tab"
         aria-controls={href}
         aria-selected={active || undefined}
+        onClick={handleClick}
       >
         {title || children}
       </a>
