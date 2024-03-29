95a5e1b650f9326e2565a921ccbafa429d500bd9
Dannilo de Souza <94450796+thimowthy@users.noreply.github.com>
2024-03-29 16:44:03 -0300

    Add "*" no label de Leito para internamento

--- pages/adicionar-paciente.tsx
+++ pages/adicionar-paciente.tsx
@@ -467,7 +467,7 @@ const AdicionarPaciente = () => {
                   className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                   htmlFor="grid-last-name"
                 >
-                  Leito
+                  Leito {!paciente ? "" : "*"}
                 </label>
                 <input
                   className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
